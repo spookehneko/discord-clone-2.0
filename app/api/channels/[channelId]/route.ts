87a3cbe24a20cd1aaf891035d08f947084bb1163
TajwarSaiyeed <rjabid36@gmail.com>
2023-09-13 19:58:41 +0600

    edit channel

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -70,3 +70,87 @@ export async function DELETE(req: Request, {params}: {
         return new NextResponse("Internal Server Error", {status: 500})
     }
 }
+
+
+export async function PATCH(req: Request, {params}: {
+    params: {
+        channelId: string
+    }
+}) {
+    try {
+        /**
+         * Get the current profile
+         */
+        const profile = await currentProfile()
+        /**
+         * no logged-in user => unauthorized access, 401
+         */
+        if (!profile) {
+            return new NextResponse("Unauthorized", {status: 401})
+        }
+        /**
+         * name, type from req body
+         * search params => serverId
+         */
+        const {name, type} = await req.json()
+        const {searchParams} = new URL(req.url)
+        const serverId = searchParams.get("serverId");
+        /**
+         * no server id => server id missing , 400
+         */
+        if (!serverId) {
+            return new NextResponse('Server id missing', {status: 400})
+        }
+        /**
+         * no params channel id => channel id missing, 400
+         */
+        if (!params.channelId) {
+            return new NextResponse('Channel id missing', {status: 400})
+        }
+        /**
+         * check if the name is 'general'
+         */
+        if (name === 'general') {
+            return new NextResponse("Name Cannot be 'general'.", {status: 400})
+        }
+
+        /**
+         * update the channel of the server
+         */
+        const server = await db.server.update({
+            where: {
+                id: serverId,
+                members: {
+                    some: {
+                        profileId: profile.id,
+                        role: {
+                            in: [MemberRole.MODERATOR, MemberRole.ADMIN]
+                        }
+                    }
+                }
+            },
+            data: {
+                channels: {
+                    update: {
+                        where: {
+                            id: params.channelId,
+                            NOT: {
+                                name: "general"
+                            }
+                        },
+                        data: {
+                            name,
+                            type
+                        }
+                    }
+                }
+            }
+        })
+
+        return NextResponse.json(server, {status: 200})
+
+    } catch (error) {
+        console.log("[Channel_ID_PATCH]", error);
+        return new NextResponse("Internal Server Error", {status: 500})
+    }
+}
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -9,6 +9,7 @@ import {CreateChannelModal} from "@/components/modals/create-channel-modal";
 import {LeaveServerModal} from "@/components/modals/leave-server-modal";
 import {DeleteServerModal} from "@/components/modals/delete-server-modal";
 import {DeleteChannelModal} from "@/components/modals/delete-channel-modal";
+import {EditChannelModal} from "@/components/modals/edit-channel-modal";
 
 
 export const ModalProvider = () => {
@@ -33,6 +34,7 @@ export const ModalProvider = () => {
             <LeaveServerModal/>
             <DeleteServerModal/>
             <DeleteChannelModal/>
+            <EditChannelModal/>
         </>
     )
 }
